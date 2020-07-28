76a25edc03be0eb473118dd0e721c33b6bedcca1
Sam Allen <allensam88@gmail.com>
2020-07-27 19:21:55 -0700

    fixed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,18 @@ module.exports = {
           },
         ],
       },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
+      },
     }
   ],
 };
