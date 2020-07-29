4ce1bc0a14b66b79a75fcdd8ec67bf807e572c02
Sam Allen <allensam88@gmail.com>
2020-07-29 15:40:29 -0700

    changed background

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful starter",
+    title: "Release | Blog",
   },
   pathPrefix: "/gatsby-contentful-starter",
   plugins: [
